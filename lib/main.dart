b188d86cd2b55d9f6d52c9a7b128b05377aa5a3a
Izmoreno <buck8159>
2021-12-02 11:41:42 +0100

    Modify Dark mode gestion with a better function.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_project/authentication_service.dart';
+import 'package:flutter_project/theme_service.dart';
 import 'package:provider/provider.dart';
 
 import 'authentification_wrapper.dart';
@@ -12,43 +13,37 @@ Future<void> main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  MyApp({Key? key}) : super(key: key);
 
-  @override
-  _MyAppState createState() => _MyAppState();
-}
+  final _notifier =
+      ValueNotifier<ThemeService>(ThemeService(mode: ThemeMode.light));
 
-class _MyAppState extends State<MyApp> {
-  bool isDark = false;
-
-  refreshDarkMode() {
-    setState(() {
-      isDark = !isDark;
-    });
-  }
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        Provider<AuthenticationService>(
-          create: (_) => AuthenticationService(FirebaseAuth.instance),
-        ),
-        StreamProvider<User?>(
-          create: (context) =>
-          context.read<AuthenticationService>().authStateChanges,
-          initialData: null,
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Welcome',
-        theme: isDark? ThemeData.dark() : ThemeData.light(),
-        home:  AuthenticationWrapper(changeDarkMode: refreshDarkMode),
-      ),
-    );
+        providers: [
+          Provider<AuthenticationService>(
+            create: (_) => AuthenticationService(FirebaseAuth.instance),
+          ),
+          StreamProvider<User?>(
+            create: (context) =>
+                context.read<AuthenticationService>().authStateChanges,
+            initialData: null,
+          ),
+          ChangeNotifierProvider<ThemeService>(create: (_) => ThemeService())
+        ],
+        child: Consumer<ThemeService>(
+          builder: (_, model, __) {
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Welcome',
+              theme: ThemeData.light(),
+              darkTheme: ThemeData.dark(),
+              themeMode: model.mode,
+              home: AuthenticationWrapper(),
+            );
+          },
+        ));
   }
 }
-
-
-
---
+++ lib/theme_service.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class ThemeService with ChangeNotifier {
+  ThemeMode _mode;
+
+  ThemeMode get mode => _mode;
+
+  ThemeService({ThemeMode mode = ThemeMode.light}) : _mode = mode;
+
+  void toggleMode() {
+    _mode = _mode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+
+    notifyListeners();
+  }
+}
