33f31a4448b1ec843aa61f39384b5edc8c334d75
Jonathan <ordred43@gmail.com>
2021-12-04 09:54:28 +0100

    Made homepage stateful so it updates automatically each time the db changes (new entry) Changed first collection entry to last entry to get the correct data

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -5,13 +5,19 @@ import 'package:flutter_project/navdrawer.dart';
 import 'package:flutter_project/theme_color.dart';
 import 'package:weather_icons/weather_icons.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<StatefulWidget> createState() {
+    return _ListState();
+  }
 
+}
+
+class _ListState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("Connected t-shirt"),
@@ -23,71 +29,68 @@ class HomePage extends StatelessWidget {
                     fontWeight: FontWeight.w600, fontSize: 40.0)),
             Expanded(
               child:
-              list(context),
+              StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance.collection("/User/29.11.2021/tshirt").snapshots(),
+                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (!snapshot.hasData) return const LinearProgressIndicator();
+                  return ListView(
+                      itemExtent: 200.0,
+                      children: <Widget> [
+                        Card(
+                            color: SecondaryColor,
+                            child: ListTile(
+                                leading: const Icon(
+                                    Icons.favorite,
+                                    color: Colors.red,
+                                    size: 150.0),
+                                title: const Text("Frequency",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500, fontSize: 18.0),),
+                                subtitle: Center(child: Text(snapshot.data!.docs.last['frequence'].toString()+" Hz",
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.w500, fontSize: 60.0),)
+                                )
+                            )),
+                        Card(
+                            color: SecondaryColor,
+                            child: ListTile(
+                                leading: const Icon(
+                                    Icons.ac_unit,
+                                    color: Colors.lightBlueAccent,
+                                    size: 150.0),
+                                title: const Text("Temperature",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500, fontSize: 18.0),),
+                                subtitle: Center(child: Text(snapshot.data!.docs.last['temperature'].toString()+" °C",
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.w500, fontSize: 60.0),)
+                                )
+                            )),
+                        Card(
+                            color: SecondaryColor,
+                            child: ListTile(
+                                leading: const Icon(
+                                    Icons.water,
+                                    color: Colors.blue,
+                                    size: 150.0),
+                                title: const Text("Humidity",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500, fontSize: 18.0),),
+                                subtitle: Center(child: Text(snapshot.data!.docs.last['humidity'].toString() +" %",
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.w500, fontSize: 60.0),)
+                                )
+                            )),
+
+                      ]
+                  );
+                }
+            )
             )]),
       drawer: const NavDrawer(),
     );
   }
 
-  Widget list(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection("/User/29.11.2021/tshirt").limit(1).snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) return const LinearProgressIndicator();
-          return ListView(
-              itemExtent: 200.0,
-              children: <Widget> [
-                Card(
-                    color: SecondaryColor,
-                    child: ListTile(
-                        leading: const Icon(
-                            Icons.favorite,
-                            color: Colors.red,
-                            size: 150.0),
-                        title: const Text("Frequency",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500, fontSize: 18.0),),
-                        subtitle: Center(child: Text(snapshot.data!.docs.first['frequence'].toString()+" Hz",
-                          style: const TextStyle(
-                              fontWeight: FontWeight.w500, fontSize: 60.0),)
-                        )
-                    )),
-                Card(
-                    color: SecondaryColor,
-                    child: ListTile(
-                        leading: const Icon(
-                            Icons.ac_unit,
-                            color: Colors.lightBlueAccent,
-                            size: 150.0),
-                        title: const Text("Temperature",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500, fontSize: 18.0),),
-                        subtitle: Center(child: Text(snapshot.data!.docs.first['temperature'].toString()+" °C",
-                          style: const TextStyle(
-                              fontWeight: FontWeight.w500, fontSize: 60.0),)
-                        )
-                    )),
-                Card(
-                    color: SecondaryColor,
-                    child: ListTile(
-                        leading: const Icon(
-                            Icons.water,
-                            color: Colors.blue,
-                            size: 150.0),
-                        title: const Text("Humidity",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500, fontSize: 18.0),),
-                        subtitle: Center(child: Text(snapshot.data!.docs.first['humidity'].toString() +" %",
-                          style: const TextStyle(
-                              fontWeight: FontWeight.w500, fontSize: 60.0),)
-                        )
-                    )),
-
-              ]
-          );
-        }
-    );
-  }
 }
 
 
