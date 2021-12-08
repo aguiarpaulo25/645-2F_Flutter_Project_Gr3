f7f9e2b04d309a69a71434317a51e0c119993c91
g.cathy <cathy.gay@students.hevs.ch>
2021-12-08 10:54:29 +0100

    Change design of current info page, add colors to theme

--- lib/pages/homepage/current_info.dart
+++ lib/pages/homepage/current_info.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter_project/pages/menu/navdrawer.dart';
-
+import 'package:flutter_project/widgets/current_info_card.dart';
 
 class CurrentInfo extends StatefulWidget {
   const CurrentInfo({Key? key}) : super(key: key);
@@ -19,71 +19,48 @@ class _CurrentInfoState extends State<CurrentInfo> {
         backgroundColor: Theme.of(context).primaryColor,
         title: const Text("Connected t-shirt"),
       ),
-      body: Column(
-          children: [
-            const Text("Welcome User",
-                style: TextStyle(
-                    fontWeight: FontWeight.w600, fontSize: 40.0)),
-            Expanded(
-                child:
-                StreamBuilder<QuerySnapshot>(
-                    stream: FirebaseFirestore.instance.collection("/User/29.11.2021/tshirt").snapshots(),
-                    builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                      if (!snapshot.hasData) return const LinearProgressIndicator();
-                      return ListView(
-                          itemExtent: 200.0,
-                          children: <Widget> [
-                            Card(
-                                color: Theme.of(context).primaryColorLight,
-                                child: ListTile(
-                                    leading: const Icon(
-                                        Icons.favorite,
-                                        color: Colors.red,
-                                        size: 150.0),
-                                    title: const Text("Frequency",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w500, fontSize: 18.0),),
-                                    subtitle: Center(child: Text(snapshot.data!.docs.last['frequence'].toString()+" Hz",
-                                      style: const TextStyle(
-                                          fontWeight: FontWeight.w500, fontSize: 60.0),)
-                                    )
-                                )),
-                            Card(
-                                color: Theme.of(context).primaryColorLight,
-                                child: ListTile(
-                                    leading: const Icon(
-                                        Icons.ac_unit,
-                                        color: Colors.lightBlueAccent,
-                                        size: 150.0),
-                                    title: const Text("Temperature",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w500, fontSize: 18.0),),
-                                    subtitle: Center(child: Text(snapshot.data!.docs.last['temperature'].toString()+" °C",
-                                      style: const TextStyle(
-                                          fontWeight: FontWeight.w500, fontSize: 60.0),)
-                                    )
-                                )),
-                            Card(
-                                color: Theme.of(context).primaryColorLight,
-                                child: ListTile(
-                                    leading: const Icon(
-                                        Icons.water,
-                                        color: Colors.blue,
-                                        size: 150.0),
-                                    title: const Text("Humidity",
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.w500, fontSize: 18.0),),
-                                    subtitle: Center(child: Text(snapshot.data!.docs.last['humidity'].toString() +" %",
-                                      style: const TextStyle(
-                                          fontWeight: FontWeight.w500, fontSize: 60.0),)
-                                    )
-                                )),
+      body: Column(children: [
+        const Padding(padding: EdgeInsets.only(top: 10.0)),
+        Text("Hello User, here is your current info",
+            style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 20.0,
+                color: Theme.of(context).primaryColorDark)),
+        Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection("/User/29.11.2021/tshirt")
+                    .snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (!snapshot.hasData) return const LinearProgressIndicator();
 
-                          ]
-                      );
-                    }
-                )
-            )]),
+                  return ListView(itemExtent: 190.0, children: <Widget>[
+                    CurrentInfoCard(
+                        cardTitle: 'Heart Frequency',
+                        cardIcon: Icons.favorite_border_outlined,
+                        connectionToDb: snapshot.data!.docs.last['frequence']
+                            .toString() +
+                            " Hz",
+                        cardColor: Theme.of(context).primaryColorLight),
+
+                    CurrentInfoCard(
+                        cardTitle: 'Temperature',
+                        cardIcon:Icons.thermostat_outlined,
+                        connectionToDb: snapshot.data!.docs.last['temperature']
+                            .toString() +
+                            " °C",
+                        cardColor: Theme.of(context).cardColor),
+
+                    CurrentInfoCard(
+                        cardTitle: 'Humidity',
+                        cardIcon:Icons.opacity_outlined,
+                        connectionToDb: snapshot.data!.docs.last['humidity'].toString() +
+                            " %",
+                        cardColor: Theme.of(context).secondaryHeaderColor),
+                  ]);
+                }))
+      ]),
       drawer: const NavDrawer(),
     );
   }
