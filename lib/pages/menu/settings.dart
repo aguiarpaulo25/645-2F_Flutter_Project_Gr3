7a34538a2572bc5a47cd703b18186d10cbac5d08
Izmoreno <buck8159>
2021-12-21 18:30:26 +0100

    Correct the theme_colors.dart. The signin_page.dart and the signup_page.dart have now the correct color implemented.
    Add the language changer for english and french. Add the dropdownbutton in the settings.dart to change the language.

--- lib/pages/menu/settings.dart
+++ lib/pages/menu/settings.dart
@@ -1,4 +1,7 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:easy_localization/src/public_ext.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_project/utils/theme_colors.dart';
 import 'package:flutter_project/utils/theme_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -10,6 +13,11 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  String dropDownValue = "English";
+  List<String> dropDownList = [
+    'English',
+    'Français',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -18,24 +26,59 @@ class _SettingsState extends State<Settings> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
-        title: const Text("Settings"),
+        title: const Text("Settings").tr(),
       ),
       body: Column(
         children: <Widget>[
           const ListTile(
-            title: Text("Choose your preferences", style: TextStyle(fontWeight: FontWeight.bold)),
+            title: Text("Choose your preferences",
+                style: TextStyle(fontWeight: FontWeight.bold)),
           ),
           SwitchListTile(
             title: const Text("Dark mode"),
             value: theme.isDarkMode,
             onChanged: (value) {
-              final provider = Provider.of<ThemeProvider>(context, listen: false);
+              final provider =
+                  Provider.of<ThemeProvider>(context, listen: false);
               provider.toggleMode(value);
             },
           ),
           const ListTile(
             title: Text("Language"),
-          )
+          ),
+          DropdownButton<String>(
+            value: dropDownValue,
+            elevation: 10,
+            underline: Container(
+              height: 2,
+              color: ThemeColors.lightTheme.primaryColor,
+            ),
+            onChanged: (String? newValue) {
+              setState(() {
+                dropDownValue = newValue!;
+
+                switch (newValue) {
+                  case "English":
+                    EasyLocalization.of(context)!
+                        .setLocale(const Locale('en', 'US'));
+                    break;
+                  case "Français":
+                    EasyLocalization.of(context)!
+                        .setLocale(const Locale('fr', 'FR'));
+                    break;
+                  default:
+                    EasyLocalization.of(context)!
+                        .setLocale(const Locale('en', 'US'));
+                }
+              });
+            },
+            items: dropDownList.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
         ],
       ),
     );
